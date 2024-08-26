90b2c5888f12c9acd5765c60cefe673f95ff0384
ValeraVasilevsky <valera200208@gmail.com>
2024-08-26 22:32:00 +0700

    feat: added layouts & not-found page

--- src/app/router.ts
+++ src/app/router.ts
@@ -3,8 +3,25 @@ import {
   createWebHistory,
   type RouteRecordRaw,
 } from "vue-router";
+import type { Component } from "vue";
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [] as RouteRecordRaw[],
+  routes: [
+    {
+      path: "/",
+      component: (): Component => import("pages/layout.vue"),
+    },
+    {
+      path: "/",
+      component: (): Component => import("pages/default.vue"),
+      children: [
+        {
+          path: "/:catchAll(.*)",
+          name: "404",
+          component: (): Component => import("pages/NotFound.vue"),
+        },
+      ],
+    },
+  ] as RouteRecordRaw[],
 });
