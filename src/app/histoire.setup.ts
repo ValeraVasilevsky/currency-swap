431b9c2e437216363ad3b7aaa618815a052b00a0
ValeraVasilevsky <valera200208@gmail.com>
2024-08-26 22:50:54 +0700

    fix: update import

--- src/app/histoire.setup.ts
+++ src/app/histoire.setup.ts
@@ -2,7 +2,7 @@ import { defineSetupVue3 } from "@histoire/plugin-vue";
 import { createPinia } from "pinia";
 
 import "modern-normalize/modern-normalize.css";
-import "./main.css";
+import "./style.css";
 
 export const setupVue3 = defineSetupVue3(({ app }) => {
   const pinia = createPinia();
