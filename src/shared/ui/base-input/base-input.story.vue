bb8167166fba76bb0047f06079bd12a1c7bf1078
ValeraVasilevsky <valera200208@gmail.com>
2024-08-27 00:04:34 +0700

    feat: added base-input component

---
+++ src/shared/ui/base-input/base-input.story.vue
@@ -0,0 +1,14 @@
+<template>
+  <Story class="inputs">
+    <BaseInput v-model="inputValue" placeholder="Placeholder" />
+  </Story>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import BaseInput from "./index.vue";
+
+const inputValue = ref<string>("");
+</script>
+
+<style scoped></style>
